7902652e9c31ec910030add25ad27d7af11a7bf0
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-06-27 10:11:03 +0530

    week3 added

---
+++ week3/auth3-1.js
@@ -0,0 +1,35 @@
+// dumbass way of doing input validation and auth
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+    // do health checks here
+    const kidneyId = parseInt(req.query.kidneyId);
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    if(kidneyId != 1 && kidneyId !=2 ) { // input validation
+        res.status(411).json({
+            msg: "wrong inputs",
+        });
+        return;
+    }
+
+    if(username === "aryan" && password === "Venom@54321") {
+        res.status(200).json({
+            msg: "You exist in the dbms!"
+        });
+        return;
+    }
+
+    if(username != "aryan" || password != "Venom@54321") { // username check
+        res.status(403).json({
+            msg: "User does not exist!",
+        });
+        return;
+    }
+
+    res.send("You kidney is healthy!");
+});
+
+app.listen(3000);
