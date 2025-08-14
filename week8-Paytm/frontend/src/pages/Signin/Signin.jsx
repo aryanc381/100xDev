79edb75a509e0ae5ef5e08b5fa2c637805f5b915
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-08-14 20:58:48 +0530

    Week8-Paytm Sign In Frontend + Backend + Linking done.

--- week8-Paytm/frontend/src/pages/Signin/Signin.jsx
+++ week8-Paytm/frontend/src/pages/Signin/Signin.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Signin() {
+  const [signPass, setSignPass] = useState('');
+  const [signUser, setSignUser] = useState('');
+
+  async function signInHandler() {
+    console.log("Sign Up data: ", { signUser, signPass });
+    const response = await axios({
+      url: "http://localhost:3000/v1/api/user/signin",
+      method: "POST",
+      data: {
+        username: signUser,
+        password: signPass
+      }
+    });
+    console.log(response);
+  }
   return (
-    <div>Signin</div>
+    <>
+        <div>
+            <div className="w-[20vw] rounded block center bg-gray-100 border border-gray-300 text-sm tracking-[-0.05rem] ml-110 mr-110 mt-20">
+                <h1 className='text-3xl p-3 text-center'>Sign In</h1>
+
+                <p className='pl-[2vw]'>Username</p>
+                <input value={signUser} onChange={(e) => {setSignUser(e.target.value)}} type="text" className='border rounded mb-[1vw] border-gray-400 ml-[2vw]'/>
+                
+                <p className='pl-[2vw]'>Password</p>
+                <input value={signPass} onChange={(e) => {setSignPass(e.target.value)}} ype="text" className='border rounded mb-[1vw] border-gray-400 ml-[2vw]'/>
+
+                <p onClick={signInHandler} className='p-[1vw] rounded-b mt-[1vw] bg-gray-200 hover:bg-black hover:cursor-pointer hover:text-white transition ease-in-out duration-200 text-center'>Sign In</p>
+            </div>
+        </div>
+    </>
   )
 }
 
