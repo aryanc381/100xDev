0d3c00744ddf47b836e34dcdbe905c061d7934d7
Aryan Chauhan <chauhanaryan381@gmail.com>
2025-11-04 18:34:26 +0530

    week16 done.

--- week12/advanceTS/src/partial.ts
+++ week12/advanceTS/src/partial.ts
@@ -1,15 +1,15 @@
-// pick allows you to create new types by selecting a set of properties from an existing type.
+// parital makes all properties of a type optional, it's basically creating a type with the same properties but marked as optional 
 interface User {
-    id: number;
-    name: string;
-    age: number;
-    createdAt: string;
+    id?: number;
+    name?: string;
+    age?: number;
+    createdAt?: string;
 }
 
 type UserBasic = Pick<User, 'name' | 'age'>
 
 function sumOfAge(user1: UserBasic, user2: UserBasic) {
-    return user1.age + user2.age;
+    return user1.age! + user2.age!;
 }
 
 const age = sumOfAge({ name: 'Varo', age: 20}, { name: 'Aryan', age: 18 });
